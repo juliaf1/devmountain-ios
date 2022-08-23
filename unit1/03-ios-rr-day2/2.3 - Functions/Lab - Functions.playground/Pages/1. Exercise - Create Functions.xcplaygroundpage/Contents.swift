f608dcc1607dad8914aa26b51f11e7dbaa1d605a
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-23 17:05:16 -0300

    unit1: adds solution to 03 exercise

--- unit1/03-ios-rr-day2/2.3 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ unit1/03-ios-rr-day2/2.3 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,26 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
-
-
+func introduceMyself() {
+    print("I'm Julia, a developer based in Rio de Janeiro who loves surfing on sunny days.")
+}
+introduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
 
-
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...6)
+    switch randomNum {
+        case ..<4: print("That was low...")
+        case 4...5: print("That's an average score")
+        case 6: print("You got the highest score!")
+        default: print("")
+    }
+}
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- unit1/03-ios-rr-day2/2.3 - Functions/Lab - Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
+++ unit1/03-ios-rr-day2/2.3 - Functions/Lab - Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
@@ -7,11 +7,19 @@
 
  As an example, write a function that only does a portion of what your previous `pacing` function did. This function will be called `calculatePace`. It should take three `Double` arguments called `currentDistance`, `totalDistance`, and `currentTime`, and should return a `Double` that will represent the time at which the user will finish the run based on the user's current distance and time. call the function and print the return value.
  */
-
-
+func calculatePace(currentDistance: Double, totalDistance: Double, currentTime: Double) -> Double {
+    (currentTime * totalDistance) / currentDistance
+}
+calculatePace(currentDistance: 200, totalDistance: 10, currentTime: 5)
 //:  Now write a function called `pacing` that takes four `Double` arguments called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. The function should also return a `String`, which will be the message to show the user. The function should call `calculatePace`, passing in the appropriate values, and capture the return value. The function should then compare the returned value to `goalTime` and if the user is on pace return "Keep it up!", and return "You've got to push it just a bit harder!" otherwise. Call the function and print the return value.
-
-
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let averageTime = calculatePace(currentDistance: currentDistance, totalDistance: totalDistance, currentTime: currentTime)
+    if averageTime > goalTime {
+        print("You've got to push it just a bit harder!")
+    } else {
+        print("Keep it up!")
+    }
+}
 /*:
  _Copyright © 2021 Apple Inc._
 
