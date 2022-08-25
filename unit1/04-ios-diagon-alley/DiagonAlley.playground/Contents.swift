b4e7e2377c807560d460084236d2c274afc13789
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-24 23:19:25 -0300

    unit1: adds diagon alley challenge part 3 and 4

--- unit1/04-ios-diagon-alley/DiagonAlley.playground/Contents.swift
+++ unit1/04-ios-diagon-alley/DiagonAlley.playground/Contents.swift
@@ -40,6 +40,21 @@ class Wizard: Gringotes {
         self.vaultGoldVolume = vaultGoldVolume
     }
     
+    func sortingHat() {
+        print("You begin to daydream about the four houses of Hogwarts. Slytherin for the cunning and ambitious, Gryffindor for the brave, Hufflepuff for the Loyal, and Ravenclaw for those with great wit. What could be the specific thought process and logic which the sorting hat employs in order to appropriately place students?")
+        
+        var chosenHouse: String {
+            switch self.trait {
+                case .loyal: return "Hufflepuff"
+                case .intelligent: return "Ravenclaw"
+                case .ambitious: return "Slytherin"
+                case .brave: return "Gryffindor"
+            }
+        }
+
+        print("\(name), you are a very \(trait) wizard. You belong in \(chosenHouse)!\n")
+    }
+    
     func spendGold(_ amount: Double) {
         vaultGoldVolume -= amount
     }
@@ -50,6 +65,15 @@ class Wizard: Gringotes {
 }
 
 struct DiagonAlley {
+    enum HoneydukesPrices {
+        static let fizzingWhizzbees: Double = 2
+        static let chocolateFrog: Double = 3
+    }
+
+    static let owls = [["beak": "brown", "feathers": "white"], ["beak": "black", "feathers": "brown"], ["beak": "black", "feathers": "black"], ["beak": "black", "feathers": "white"]]
+    
+    static let brooms = [["sponsor": "Chudley Cannons"], ["sponsor": "Falmouth Falcons"], ["sponsor": "The Holyhead Harpies"], ["sponsor": "Bigonville Bombers"]]
+
     static func buyWandAtOllivanders(wizard: Wizard, amount: Double) {
         print("You walk into Ollivanders to buy your first wand. The cost is \(amount) galleons.\n")
 
@@ -97,6 +121,50 @@ struct DiagonAlley {
             return true
         }
     }
+    
+    static func buyChocolateFrogsAtHoneydukes(wizard: Wizard, units: Int) {
+        print("There's no better place than Honeydukes to get your chocolate frogs.\n")
+        
+        let totalPrice = HoneydukesPrices.chocolateFrog * Double(units)
+
+        if hasEnoughFunds(wizard: wizard, amount: totalPrice) {
+            wizard.spendGold(totalPrice)
+            print("I bet you got Dumbledore's card!\n")
+        }
+    }
+    
+    static func buyfizzingWhizzbeesAtHoneydukes(wizard: Wizard, units: Int) {
+        print("The Fizzing Whizzbees at Honeydukes are the best - you can't leave without one of those!\n")
+        
+        let totalPrice = HoneydukesPrices.fizzingWhizzbees * Double(units)
+
+        if hasEnoughFunds(wizard: wizard, amount: totalPrice) {
+            wizard.spendGold(totalPrice)
+            print("There you go, have fun with your treat!\n")
+        }
+    }
+    
+    static func chooseOwl(beakColor: String, feathersColor: String) {
+        print("I'm looking for an owl with \(beakColor) beak and \(feathersColor) feathers")
+        
+        for owl in owls {
+            if owl["beak"]! == beakColor && owl["feathers"]! == feathersColor {
+                print("This one's mine!\n")
+            } else {
+                print("Pass")
+            }
+        }
+    }
+    
+    static func chooseBroom(team: String) {
+        print("I heard that my favorite Quidditch team, \(team), suggests a certain type of Broom. So, I will only buy a broom if it is sponsored by them!")
+
+        for broom in brooms {
+            if broom["sponsor"]! == team {
+                print("Brilliant, this broom amazing!\n")
+            }
+        }
+    }
 }
 
 var julia = Wizard(firstName: "Julia", lastName: "Granger", age: 24, birthday: "10/20/1997", preferredWandLength: 12, preferredHouse: "Ravenclaw", trait: .loyal, tiredOfHarryPotterReferences: false, vaultGoldVolume: 150.0)
@@ -110,3 +178,13 @@ DiagonAlley.receiveGoldFromLongLostRelative(wizard: julia, amount: 300)
 DiagonAlley.receiveGoldPlayingWizardDice(wizard: julia)
 
 DiagonAlley.buySuspiciousArtefactsAtBorginBurkes(wizard: julia, deal: 10)
+
+DiagonAlley.buyfizzingWhizzbeesAtHoneydukes(wizard: julia, units: 3)
+
+DiagonAlley.buyChocolateFrogsAtHoneydukes(wizard: julia, units: 5)
+
+DiagonAlley.chooseOwl(beakColor: "black", feathersColor: "white")
+
+DiagonAlley.chooseBroom(team: "The Holyhead Harpies")
+
+julia.sortingHat()
