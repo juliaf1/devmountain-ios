bb44e0cde4f04abba4ba2d3af6b041ca71c89a33
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-26 18:12:43 -0300

    unit1: adds outlets and data to view controller

--- unit1/06-ios-my-first-app/PetProfile/PetProfile/ViewController.swift
+++ unit1/06-ios-my-first-app/PetProfile/PetProfile/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        petSetup()
+    }
+    
+    func petSetup() {
+        nameLabel.text = "Peppa"
+        titleLabel.text = "Dinousaur in dog disguise, Trouble maker, 10 Times Fastest Eater Champion"
+        ageLabel.text = "1 year"
+        imageView.image = UIImage(named: "peppa")
+        bioLabel.text = "Peppa is a one year old Golden Retriever pup who loves eating, jumping, playing, cuddling and destroying all types of objects, especially cloths. She enjoys bananas, coconuts, water, water playing, pools, sand, trees and people. But don't let her fool you, she has gigantic dinousaur paws."
     }
 
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
 }
 
