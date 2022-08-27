bc3733d8b3bbe1a8392646b40b2ba623accad4a8
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-27 13:28:41 -0300

    unit1: adds planet model class to solar system exercise 08

--- unit1/08-ios-solar-system/SolarSystem/SolarSystem/Models/Planet.swift
+++ unit1/08-ios-solar-system/SolarSystem/SolarSystem/Models/Planet.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class Planet {
+    let name: String
+    let imageName: String
+    let diameter: Int
+    let dayLength: Double
+    let maxMillionKMsFromSun: Double
+    
+    init(_ name: String, imageName: String, diameter: Int, dayLength: Double, maxMillionKMsFromSun: Double) {
+        self.name = name
+        self.imageName = imageName
+        self.diameter = diameter
+        self.dayLength = dayLength
+        self.maxMillionKMsFromSun = maxMillionKMsFromSun
+    }
+}
