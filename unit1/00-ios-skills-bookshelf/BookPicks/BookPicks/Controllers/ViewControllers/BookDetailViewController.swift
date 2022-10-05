551b17c792eb9bb17a0151a4bb457b16c626f168
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-05 15:42:46 -0300

    unit1: sets up book list table view

---
+++ unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/Controllers/ViewControllers/BookDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookDetailViewController.swift
+//  BookPicks
+//
+//  Created by Julia Frederico on 05/10/22.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/Controllers/ViewControllers/BookListTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BookListTableViewController.swift
+//  BookPicks
+//
+//  Created by Julia Frederico on 05/10/22.
+//
+
+import UIKit
+
+class BookListTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+
+    let books = BookController().books
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath)
+
+        let book = books[indexPath.row]
+        cell.textLabel?.text = book.title
+        cell.detailTextLabel?.text = book.author
+        cell.imageView?.image = UIImage(named: book.photo)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
