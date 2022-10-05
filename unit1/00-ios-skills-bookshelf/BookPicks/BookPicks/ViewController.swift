9e96d36ed2d0fa09b6002815daa040926143d2a4
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-05 12:10:35 -0300

    unit1: adds book picks project with MVC structure

---
+++ unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookPicks
+//
+//  Created by Julia Frederico on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
