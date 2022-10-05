addb119bfa3f828a7cea6e233e7bf5b5bb4e3c90
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-05 12:55:40 -0300

    unit1: adds book controller and convenience release date initializer

---
+++ unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/Controllers/ModelControllers/BookController.swift
@@ -0,0 +1,56 @@
+//
+//  BookController.swift
+//  BookPicks
+//
+//  Created by Julia Frederico on 05/10/22.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] {
+        let b1 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+        
+        let b2 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+        
+        let b3 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+        
+        let b4 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+        
+        let b5 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+        
+        let b6 = Book(title: "Anarquistas Graças a Deus",
+                      author: "Zelia Gattai",
+                      description: "",
+                      stringDate: "",
+                      photo: "anarquistas",
+                      rating: 5)
+
+        return [b1, b2, b3, b4, b5, b6]
+    }
+}
--- unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/Models/Book.swift
+++ unit1/00-ios-skills-bookshelf/BookPicks/BookPicks/Models/Book.swift
@@ -9,14 +9,40 @@ import Foundation
 
 class Book {
     let title: String
+    let author: String
     let description: String
+    let releaseDate: Date
     let photo: String
     var rating: Int
     
-    init(title: String, description: String, photo: String, rating: Int) {
+    var formattedDate: String {
+        return formatDate(fromDate: releaseDate)
+    }
+
+    init(title: String, author: String, description: String, releaseDate: Date, photo: String, rating: Int) {
         self.title = title
+        self.author = author
         self.description = description
+        self.releaseDate = releaseDate
         self.photo = photo
         if rating >= 5 { self.rating = 5 } else { self.rating = rating }
     }
+    
+    convenience init(title: String, author: String, description: String, stringDate: String, photo: String, rating: Int) {
+        let date = formatDate(fromString: stringDate)
+        self.init(title: title, author: author, description: description, releaseDate: date, photo: photo, rating: rating)
+    }
+}
+
+func formatDate(fromString dateString: String) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd-MM-yyy"
+    guard let date = dateFormatter.date(from: dateString) else { return Date() }
+    return date
+}
+
+func formatDate(fromDate date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd-MM-yyy"
+    return dateFormatter.string(from: date)
 }
