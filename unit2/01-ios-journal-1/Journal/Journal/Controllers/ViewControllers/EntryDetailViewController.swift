869f5038ef74642f63c7120afd2840c275dfe2ba
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-13 18:31:36 -0300

    feat(unit2-journal1): (adds entry detail view controller)

---
+++ unit2/01-ios-journal-1/Journal/Journal/Controllers/ViewControllers/EntryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Julia Frederico on 13/10/22.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
