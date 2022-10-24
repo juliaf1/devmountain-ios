991ca6457d277e4d87977c77103d5f959fef6a32
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-24 16:35:41 -0300

    chore(unit2-task): (create project)

---
+++ unit2/02-ios-task/Task/Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Julia Frederico on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
