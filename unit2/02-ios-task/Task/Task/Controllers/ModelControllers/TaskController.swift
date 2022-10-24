37d16a04e453abb391ffb5b1eb78a40198c886bc
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-24 16:45:54 -0300

    feat(unit2-task): (adds crud actions to task model controller)

--- unit2/02-ios-task/Task/Task/Controllers/ModelControllers/TaskController.swift
+++ unit2/02-ios-task/Task/Task/Controllers/ModelControllers/TaskController.swift
@@ -17,4 +17,18 @@ class TaskController {
     
     // MARK: - CRUD
     
+    func createTask(title: String, deadline: Date) {
+        let task = Task(title: title, deadline: deadline)
+        tasks.append(task)
+    }
+    
+    func completeTask(_ task: Task) {
+        task.completed = true
+    }
+    
+    func deleteTask(_ task: Task) {
+        guard let index = tasks.firstIndex(of: task) else { return }
+        tasks.remove(at: index)
+    }
+    
 }
