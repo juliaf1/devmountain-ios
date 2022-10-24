81178db9985a99c78068ff3675c9403ae6b86b66
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-24 16:38:35 -0300

    feat(unit2-task): (sets up MVC folder and files structure)

---
+++ unit2/02-ios-task/Task/Task/Controllers/ModelControllers/TaskController.swift
@@ -0,0 +1,12 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Julia Frederico on 24/10/22.
+//
+
+import Foundation
+
+class TaskController {
+    
+}
---
+++ unit2/02-ios-task/Task/Task/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Julia Frederico on 24/10/22.
+//
+
+import Foundation
+
+class Task {
+    
+}
