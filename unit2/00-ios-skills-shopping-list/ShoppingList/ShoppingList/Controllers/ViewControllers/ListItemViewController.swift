486e511c6bf92dc63a65324dfac9748e05b0e4a7
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-25 20:23:08 -0300

    feat(unit2-shopping): (implement add item and check/uncheck item)

---
+++ unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Controllers/ViewControllers/ListItemViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ListItemViewController.swift
+//  ShoppingList
+//
+//  Created by Julia Frederico on 25/10/22.
+//
+
+import UIKit
+
+class ListItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Properties and outlets
+    
+    let listController = ListItemController.shared
+    
+    var items: [ListItem] {
+        listController.items
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var listItemNameLabel: UITextField!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        listController.loadFromPersistentStorage()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func didPressSaveItemButton(_ sender: UIButton) {
+        guard let name = listItemNameLabel.text,
+              !name.isEmpty else { return }
+
+        listItemNameLabel.text = ""
+        listController.add(ListItem(name: name))
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view datasource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath) as? ListItemTableViewCell else { return UITableViewCell() }
+        
+        cell.delegate = self
+
+        let item = items[indexPath.row]
+        cell.updateView(for: item)
+        
+        return cell
+    }
+
+}
+
+extension ListItemViewController: ListItemCellDelegate {
+    
+    func didToggleCheck(for cell: ListItemTableViewCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else { return }
+        let item = items[indexPath.row]
+        listController.toggleCheck(of: item)
+        tableView.reloadData()
+    }
+    
+}
--- unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Views/CustomViews/ListItemTableViewCell.swift
+++ unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Views/CustomViews/ListItemTableViewCell.swift
@@ -7,8 +7,31 @@
 
 import UIKit
 
+protocol ListItemCellDelegate {
+    func didToggleCheck(for cell: ListItemTableViewCell)
+}
+
 class ListItemTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties and outlets
+    
+    @IBOutlet weak var listItemLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
 
+    var delegate: ListItemCellDelegate?
+    
+    // MARK: - Actions
 
-
+    @IBAction func didPressCheckButton(_ sender: UIButton) {
+        delegate?.didToggleCheck(for: self)
+    }
+    
+    // MARK: - Helpers
+    
+    func updateView(for item: ListItem) {
+        let checkImage = item.checked ? "checkmark.circle" : "circle"
+        listItemLabel.text = item.name
+        checkButton.setImage(UIImage(systemName: checkImage), for: .normal)
+    }
+    
 }
