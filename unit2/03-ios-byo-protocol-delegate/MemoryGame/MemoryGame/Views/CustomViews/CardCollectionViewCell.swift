f946b1460ebfd61e809ad4935e83d58a1cc2fac8
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-25 13:40:15 -0300

    feat(unit2-memorygame): (adds card collection view cell with outlets and helper functions to hide/show card)

--- unit2/03-ios-byo-protocol-delegate/MemoryGame/MemoryGame/Views/CustomViews/CardCollectionViewCell.swift
+++ unit2/03-ios-byo-protocol-delegate/MemoryGame/MemoryGame/Views/CustomViews/CardCollectionViewCell.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var cardLabel: UILabel!
+
+    func displayCard(_ card: Card) {
+        cardView.backgroundColor = .systemGray4
+        cardLabel.text = card.emoji
+        cardLabel.font = cardLabel.font.withSize(32)
+    }
     
-    
-    
+    func hideCard(_ card: Card) {
+        cardView.backgroundColor = .systemIndigo
+        cardLabel.text = "🙈"
+        cardLabel.font = cardLabel.font.withSize(12)
+    }
 }
