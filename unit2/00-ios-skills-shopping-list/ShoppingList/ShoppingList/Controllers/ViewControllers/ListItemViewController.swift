5897a044fda27a822c4777e60d39d614876792e4
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-25 20:42:15 -0300

    feat(unit2-shopping): (set unchecked/checked items table view sections)

--- unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Controllers/ViewControllers/ListItemViewController.swift
+++ unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Controllers/ViewControllers/ListItemViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class ListItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    enum Section: Int {
+        case unchecked
+        case checked
+    }
+    
     // MARK: - Properties and outlets
     
     let listController = ListItemController.shared
     
-    var items: [ListItem] {
-        listController.items
+    var uncheckedItems: [ListItem] {
+        listController.items.filter { item in !item.checked }
+    }
+    
+    var checkedItems: [ListItem] {
+        listController.items.filter { item in item.checked }
     }
     
     @IBOutlet weak var tableView: UITableView!
@@ -46,20 +55,50 @@ class ListItemViewController: UIViewController, UITableViewDelegate, UITableView
     // MARK: - Table view datasource
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
+        return 50
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch Section(rawValue: section) {
+        case .unchecked:
+            return "Items to buy"
+        case .checked:
+            return "Items bought"
+        case .none:
+            return ""
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        switch Section(rawValue: section) {
+        case .unchecked:
+            return uncheckedItems.count
+        case .checked:
+            return checkedItems.count
+        case .none:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath) as? ListItemTableViewCell else { return UITableViewCell() }
         
         cell.delegate = self
-
-        let item = items[indexPath.row]
-        cell.updateView(for: item)
+        
+        switch Section(rawValue: indexPath.section) {
+        case .unchecked:
+            let item = uncheckedItems[indexPath.row]
+            cell.updateView(for: item)
+        case .checked:
+            let item = checkedItems[indexPath.row]
+            cell.updateView(for: item)
+        case .none:
+            break
+        }
         
         return cell
     }
@@ -67,10 +106,18 @@ class ListItemViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
-            let item = items[indexPath.row]
-            listController.delete(item)
+            switch Section(rawValue: indexPath.section) {
+            case .unchecked:
+                let item = uncheckedItems[indexPath.row]
+                listController.delete(item)
+            case .checked:
+                let item = checkedItems[indexPath.row]
+                listController.delete(item)
+            case .none:
+                break
+            }
+
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            break
         case .insert:
             break
         case .none:
@@ -86,8 +133,18 @@ extension ListItemViewController: ListItemCellDelegate {
     
     func didToggleCheck(for cell: ListItemTableViewCell) {
         guard let indexPath = tableView.indexPath(for: cell) else { return }
-        let item = items[indexPath.row]
-        listController.toggleCheck(of: item)
+        
+        switch Section(rawValue: indexPath.section) {
+        case .unchecked:
+            let item = uncheckedItems[indexPath.row]
+            listController.toggleCheck(of: item)
+        case .checked:
+            let item = checkedItems[indexPath.row]
+            listController.toggleCheck(of: item)
+        case .none:
+            break
+        }
+
         tableView.reloadData()
     }
     
