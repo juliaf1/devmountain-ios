3cc2df47734e01ed85f7c7bd145fd57d9dcc7853
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-25 19:10:14 -0300

    feat(unit2-shopping): (adds view layout with list item custom table cells and text input)

---
+++ unit2/00-ios-skills-shopping-list/ShoppingList/ShoppingList/Views/CustomViews/ListItemTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ListItemTableViewCell.swift
+//  ShoppingList
+//
+//  Created by Julia Frederico on 25/10/22.
+//
+
+import UIKit
+
+class ListItemTableViewCell: UITableViewCell {
+
+
+
+}
