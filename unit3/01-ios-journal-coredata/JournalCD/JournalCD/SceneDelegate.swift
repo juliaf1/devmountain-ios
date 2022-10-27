3b2fc0c93ce6375209b77820e7769918a1a71dd4
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-27 19:00:33 -0300

    chore(unit3-journal): (removes boilerplate folder and creates new app project)

--- unit3/01-ios-journal-coredata/JournalCD/JournalCD/SceneDelegate.swift
+++ unit3/01-ios-journal-coredata/JournalCD/JournalCD/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  JournalCoreData
+//  JournalCD
 //
-//  Created by Cameron Stuart on 1/18/21.
+//  Created by Julia Frederico on 27/10/22.
 //
 
 import UIKit
@@ -45,9 +45,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
