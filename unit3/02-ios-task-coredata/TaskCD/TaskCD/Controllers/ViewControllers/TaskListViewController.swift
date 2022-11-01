dc542461e6a00573c9fd498b8c12710acec22960
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-01 00:01:33 -0300

    feat(unit3-task): (add marks to view controllers)

--- unit3/02-ios-task-coredata/TaskCD/TaskCD/Controllers/ViewControllers/TaskListViewController.swift
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Controllers/ViewControllers/TaskListViewController.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class TaskListViewController: UIViewController {
+    
+    // MARK: - Properties and outlets
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Actions
 
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
+    
+    // MARK: - Helpers
 
 }
--- unit3/02-ios-task-coredata/TaskCD/TaskCD/Resources/Helpers/Strings.swift
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Resources/Helpers/Strings.swift
@@ -10,7 +10,10 @@ import Foundation
 enum Strings {
     
     static let appName = "TaskCD"
-    
+
     static let taskEntityName = "Task"
+
+    static let incompleteTaskImageName = "square"
+    static let completeTaskImageName = "checkmark.square"
     
 }
