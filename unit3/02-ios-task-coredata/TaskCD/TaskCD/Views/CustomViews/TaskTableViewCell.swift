b461f5d1986fc1e872b52b761514e19b23021e81
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-01 00:14:28 -0300

    feat(unit3-task): (adds outlets and actions to storyboard + implements task table view cell with delegate protocol)

--- unit3/02-ios-task-coredata/TaskCD/TaskCD/Views/CustomViews/TaskTableViewCell.swift
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Views/CustomViews/TaskTableViewCell.swift
@@ -7,8 +7,33 @@
 
 import UIKit
 
+protocol TaskCellDelegate {
+    func toggleCompleteTask(for cell: TaskTableViewCell)
+}
+
 class TaskTableViewCell: UITableViewCell {
 
+    // MARK: - Properties and outlets
+    
+    @IBOutlet weak var taskNameLabel: UILabel!
+    @IBOutlet weak var taskCompleteButton: UIButton!
+    
+    var delegate: TaskCellDelegate?
+    
+    // MARK: - Actions
+    
+    @IBAction func didPressCompleteTaskButton(_ sender: UIButton) {
+        delegate?.toggleCompleteTask(for: self)
+    }
+    
+    // MARK: - Helpers
+    
+    func updateView(with task: Task) {
+        let image = UIImage(systemName: task.completed ? Strings.completeTaskImageName : Strings.incompleteTaskImageName)
+        
+        taskNameLabel.text = task.name
+        taskCompleteButton.setImage(image, for: .normal)
+    }
 
 
 }
