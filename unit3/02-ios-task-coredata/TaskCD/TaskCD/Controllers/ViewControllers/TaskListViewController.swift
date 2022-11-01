e62edc7923edd0b816bf508934f9496d14b2e927
Julia Frederico <magalhaesjulia@id.uff.br>
2022-10-31 23:58:14 -0300

    feat(unit3-task): (include view controller and custom cell view files + class VCs on storyboard)

---
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Controllers/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TaskListViewController.swift
+//  TaskCD
+//
+//  Created by Julia Frederico on 31/10/22.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
+
+}
---
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Views/CustomViews/TaskTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskTableViewCell.swift
+//  TaskCD
+//
+//  Created by Julia Frederico on 31/10/22.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+
+
+}
