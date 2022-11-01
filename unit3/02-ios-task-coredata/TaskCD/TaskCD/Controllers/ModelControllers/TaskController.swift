75fd48d51f7b5883f86f013c8a146c4300669bb9
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-01 00:15:29 -0300

    fix(unit3-task): (set shared instance as static property of task controller)

--- unit3/02-ios-task-coredata/TaskCD/TaskCD/Controllers/ModelControllers/TaskController.swift
+++ unit3/02-ios-task-coredata/TaskCD/TaskCD/Controllers/ModelControllers/TaskController.swift
@@ -11,7 +11,7 @@ class TaskController {
     
     // MARK: - Properties and shared instance
     
-    let shared = TaskController()
+    static let shared = TaskController()
     
     var completedTasks: [Task] = []
     var incompleteTasks: [Task] = []
