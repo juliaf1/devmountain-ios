05cea66b4c184c91231ba57a783a70d89d2ac17c
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-01 17:07:33 -0300

    feat(unit3-task): (implement updateView and tap enable alarm button on alarm detail VC)

--- unit3/03-ios-alarm-coredata/AlarmCoreData/AlarmCoreData/Controllers/ViewControllers/AlarmDetailTableViewController.swift
+++ unit3/03-ios-alarm-coredata/AlarmCoreData/AlarmCoreData/Controllers/ViewControllers/AlarmDetailTableViewController.swift
@@ -12,15 +12,51 @@ class AlarmDetailTableViewController: UITableViewController {
     // MARK: - Properties and outlets
     
     var alarm: Alarm?
+    var alarmEnabled = true
+    
+    @IBOutlet weak var alarmFireDatePicker: UIDatePicker!
+    @IBOutlet weak var alarmTitleField: UITextField!
+    @IBOutlet weak var alarmEnabledButton: UIButton!
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateView()
     }
     
     // MARK: - Actions
     
+    @IBAction func didTapAlarmEnabledButton(_ sender: UIButton) {
+        alarmEnabled = !alarmEnabled
+        designIsEnabledButton()
+    }
+    
+    @IBAction func didTapSaveButton(_ sender: UIBarButtonItem) {
+    }
+    
     // MARK: - Helpers
+    
+    func updateView() {
+        guard let alarm = alarm else {
+            designIsEnabledButton()
+            return
+        }
+        alarmFireDatePicker.date = alarm.fireDate!
+        alarmTitleField.text = alarm.title
+        alarmEnabled = alarm.enabled
+        designIsEnabledButton()
+    }
+    
+    func designIsEnabledButton() {
+        switch alarmEnabled {
+        case true:
+            alarmEnabledButton.backgroundColor = .white
+            alarmEnabledButton.setTitle("On", for: .normal)
+        case false:
+            alarmEnabledButton.backgroundColor = .darkGray
+            alarmEnabledButton.setTitle("Off", for: .normal)
+        }
+    }
 
 }
