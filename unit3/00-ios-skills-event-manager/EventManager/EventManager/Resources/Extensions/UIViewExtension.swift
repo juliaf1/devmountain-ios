7b1136071d8f899dc0efbc29a7b73495b3e95d61
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-09 12:56:41 -0300

    feat(unit3-event): (include ui view extension with anchor function to help constraint view)

---
+++ unit3/00-ios-skills-event-manager/EventManager/EventManager/Resources/Extensions/UIViewExtension.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewExtension.swift
+//  EventManager
+//
+//  Created by Julia Frederico on 09/11/22.
+//
+
+import UIKit
+
+extension UIView {
+
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, marginTop: CGFloat, marginBottom: CGFloat, marginLeft: CGFloat, marginRight: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: marginTop).isActive = true
+        }
+
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -marginBottom).isActive = true
+        }
+
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: marginLeft).isActive = true
+        }
+
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: -marginRight).isActive = true
+        }
+
+        if let width = width {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+        if let height = height {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+
+    }
+
+}
