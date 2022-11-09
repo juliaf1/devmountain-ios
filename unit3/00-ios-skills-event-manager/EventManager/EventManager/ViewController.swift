2fec2a4259b4ba9bf278a92975b089211e400460
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-09 12:18:48 -0300

    chore(unit3-event): (add event project)

---
+++ unit3/00-ios-skills-event-manager/EventManager/EventManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventManager
+//
+//  Created by Julia Frederico on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
