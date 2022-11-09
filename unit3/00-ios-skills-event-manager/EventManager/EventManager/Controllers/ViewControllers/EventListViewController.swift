71a7882769a9cf14e6d52713095eb1dfdd6852f1
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-09 12:37:35 -0300

    chore(unit3-event): (set up launch screen as main interface + structure event list VC)

--- unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventListViewController.swift
+++ unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventListViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class EventListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+
+        self.view.backgroundColor = .systemIndigo
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    // MARK: - Actions
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+    // MARK: - Views
+
 
 }
