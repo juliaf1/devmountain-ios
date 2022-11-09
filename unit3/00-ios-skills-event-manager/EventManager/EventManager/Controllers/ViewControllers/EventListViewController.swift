dd1fbb8d1374efb8c63d858767e96734f3f0ab03
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-09 16:59:19 -0300

    feat(unit3-event): (create and configure table view for event list vc)

--- unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventListViewController.swift
+++ unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventListViewController.swift
@@ -21,18 +21,43 @@ class EventListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpTableView()
     }
     
     // MARK: - Actions
+    
+    // MARK: - Helpers
+    
+    func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        // configure and register cell
+    }
 
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // todo
     }
     
     // MARK: - Views
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
 
+}
 
+extension EventListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
