e4c06153ff23af78845a1032d220f2a1204369eb
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-10 16:50:40 -0300

    feat(unit3-event): (implement event detail view with text field and date picker)

--- unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventDetailViewController.swift
+++ unit3/00-ios-skills-event-manager/EventManager/EventManager/Controllers/ViewControllers/EventDetailViewController.swift
@@ -13,6 +13,10 @@ class EventDetailViewController: UIViewController {
     
     var event: Event?
     
+    var safeArea: UILayoutGuide {
+        return self.view.safeAreaLayoutGuide
+    }
+    
     // MARK: - Lifecycle
     
     override func loadView() {
@@ -30,10 +34,24 @@ class EventDetailViewController: UIViewController {
     
     func setUpSubviews() {
         self.view.backgroundColor = .systemGray6
+        
+        self.view.addSubview(eventNameTextField)
+        self.view.addSubview(eventDatePicker)
+        
+        constraintEventNameTextField()
+        constraintEventDatePicker()
     }
     
     func setUpNavigationBar() {
         self.navigationController?.navigationBar.tintColor = .darkGray
+    }
+    
+    func constraintEventNameTextField() {
+        eventNameTextField.anchor(top: safeArea.topAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, marginTop: Spacings.mediumSpacing, marginBottom: 0, marginLeft: Spacings.mediumSpacing, marginRight: Spacings.mediumSpacing)
+    }
+    
+    func constraintEventDatePicker() {
+        eventDatePicker.anchor(top: eventNameTextField.bottomAnchor, bottom: safeArea.bottomAnchor, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, marginTop: Spacings.mediumSpacing, marginBottom: Spacings.mediumSpacing, marginLeft: Spacings.mediumSpacing, marginRight: Spacings.mediumSpacing)
     }
     
     // MARK: - Views
@@ -46,8 +64,22 @@ class EventDetailViewController: UIViewController {
     
     let eventNameTextField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Event name"
+        
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .systemGray5
+        textField.tintColor = .darkGray
         
         return textField
+    }()
+    
+    let eventDatePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.minuteInterval = 10
+        
+        return datePicker
     }()
 
 }
