58471a33e4c30b13fb123196c863b9446db7a2fc
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-12 16:06:43 -0300

    feat(unit4-pokedex): (add function to fetch all pokemon types)

--- unit4/03-ios-pokedex/Pokedex/Pokedex/Controllers/ModelControllers/PokemonTypeController.swift
+++ unit4/03-ios-pokedex/Pokedex/Pokedex/Controllers/ModelControllers/PokemonTypeController.swift
@@ -9,6 +9,51 @@ import Foundation
 
 class PokemonTypeController {
     
-    static let baseURL = ""
+    // MARK: - Properties
+    
+    static let baseURL = URL(string: Strings.baseURL)
+    static let typeEP = Strings.pokemonTypeEndpoint
+    
+    // MARK: - Methods
+    
+    static func pokemonTypesURL() -> URL? {
+        guard let baseURL = baseURL else { return nil }
+        let typeURL = baseURL.appendingPathComponent(typeEP)
+        return typeURL
+    }
+
+    static func fetchAllPokemonTypes(completion: @escaping (Result<[PokemonType], PokemonError>) -> Void) {
+        
+        guard let typesURL = pokemonTypesURL() else { return completion(.failure(.invalidURL)) }
+        
+        URLSession.shared.dataTask(with: typesURL) { data, response, error in
+            
+            if let error = error {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                let status = response.statusCode
+                if status != 200 {
+                    print("STATUS CODE in \(#function): \(status)")
+                }
+            }
+            
+            guard let data = data else {
+                return completion(.failure(.noData))
+            }
+            
+            do {
+                let pokemonTypesResponse = try JSONDecoder().decode(PokemonTypeResponse.self, from: data)
+                return completion(.success(pokemonTypesResponse.types))
+            } catch {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+        }.resume()
+    }
+    
+    
+    
     
 }
