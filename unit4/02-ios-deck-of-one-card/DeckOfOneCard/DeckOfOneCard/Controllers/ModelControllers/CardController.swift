3f2d68000b52d089cbe26a3c8e92b6c3e37279cb
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-12 14:03:52 -0300

    feat(unit4-deckofcards): (implement fetch one card function on card controller)

--- unit4/02-ios-deck-of-one-card/DeckOfOneCard/DeckOfOneCard/Controllers/ModelControllers/CardController.swift
+++ unit4/02-ios-deck-of-one-card/DeckOfOneCard/DeckOfOneCard/Controllers/ModelControllers/CardController.swift
@@ -12,9 +12,9 @@ class CardController {
     // MARK: - Properties
     
     static let baseURL = URL(string: "https://deckofcardsapi.com/api")
-    static let drawCardEP = "/deck/new/draw" // ?count=1
+    static let drawCardEP = "/deck/new/draw"
 
-    static var drawOneCardURL: URL? {
+    static let drawOneCardURL: URL? = {
         guard let baseURL = baseURL else { return nil }
         
         let drawCardURL = baseURL.appendingPathComponent(drawCardEP)
@@ -26,11 +26,39 @@ class CardController {
         guard let finalURL = urlComponents?.url else { return nil }
         
         return finalURL
-    }
+    }()
     
     // MARK: - Methods
     
-    static func fetchOneCard(completion: @escaping (Result<Card, Error>) -> Void) {
+    static func fetchOneCard(completion: @escaping (Result<Card, CardError>) -> Void) {
+        
+        guard let url = drawOneCardURL else { return completion(.failure(.invalidURL)) }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                let status = response.statusCode
+                if status != 200 {
+                    print("STATUS CODE in \(#function): \(status)")
+                }
+            }
+            
+            guard let data = data else {
+                return completion(.failure(.noData))
+            }
+            
+            do {
+                let card = try JSONDecoder().decode(Card.self, from: data)
+                return completion(.success(card))
+            } catch {
+                return completion(.failure(.thrownError(error)))
+            }
+                    
+        }.resume()
         
     }
     
