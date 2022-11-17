b32347da22ae18d126fb8478c443b03fd50aa620
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-17 14:17:13 -0300

    chore(unit4-moviesearch): (adds api base urls to strings constants)

--- unit4/00-ios-skills-moviesearch/MovieSearch/MovieSearch/Helpers + Extensions/Strings.swift
+++ unit4/00-ios-skills-moviesearch/MovieSearch/MovieSearch/Helpers + Extensions/Strings.swift
@@ -9,6 +9,8 @@ import Foundation
 
 enum Strings {
     
-    static let APIKey = "b0e6ffabab4753ec09f1e6e8a8338e86"
+    static let apiKey = "b0e6ffabab4753ec09f1e6e8a8338e86"
+    static let apiBaseURL = "https://api.themoviedb.org/3"
+    static let imageBaseURL = "https://image.tmdb.org/t/p/w500"
     
 }
