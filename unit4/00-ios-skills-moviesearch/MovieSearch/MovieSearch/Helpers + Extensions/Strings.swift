28d3071dff1a05419d4a0126911c25e223fb4a61
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-17 14:04:27 -0300

    chore(unit4-moviesearch): (organize app in mvc folder structure)

---
+++ unit4/00-ios-skills-moviesearch/MovieSearch/MovieSearch/Helpers + Extensions/Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Strings.swift
+//  MovieSearch
+//
+//  Created by Julia Frederico on 17/11/22.
+//
+
+import Foundation
+
+enum Strings {
+    
+    static let APIKey = "b0e6ffabab4753ec09f1e6e8a8338e86"
+    
+}
