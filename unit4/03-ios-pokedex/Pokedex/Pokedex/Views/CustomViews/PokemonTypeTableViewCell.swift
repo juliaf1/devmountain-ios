c0472735189a130e665502b2edb948dcea0eb738
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-17 12:23:49 -0300

    feat(unit4-pokedex): (include pokemon type table view on storyboard + adds poke type custom cell)

---
+++ unit4/03-ios-pokedex/Pokedex/Pokedex/Views/CustomViews/PokemonTypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTypeTableViewCell.swift
+//  Pokedex
+//
+//  Created by Julia Frederico on 17/11/22.
+//
+
+import UIKit
+
+class PokemonTypeTableViewCell: UITableViewCell {
+    
+    // MARK: - Propreties and Outlets
+
+    @IBAction func typeButton(_ sender: UIButton) {
+    }
+    
+    // MARK: - Actions
+
+    @IBAction func didPressTypeButton(_ sender: UIButton) {
+    }
+    
+    // MARK: - Helpers
+    
+}
