aafba719037c8db7f2829a52cbcc68541b7cfb2b
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-22 16:12:48 -0300

    feat(unit5-journal): (add basic view structure to entry list)

--- unit5/01-ios-journal-cloudkit/JournalCK/JournalCK/Controllers/ViewControllers/EntryListViewController.swift
+++ unit5/01-ios-journal-cloudkit/JournalCK/JournalCK/Controllers/ViewControllers/EntryListViewController.swift
@@ -7,18 +7,66 @@
 
 import UIKit
 
+// Refactor colors to helper file
+let brandColor = #colorLiteral(red: 1, green: 0.2706922889, blue: 0.2406029403, alpha: 1)
+
 class EntryListViewController: UIViewController {
     
     // MARK: - Properties
     
     // MARK: - Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        setupViews()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = .systemPink
+        configureViews()
     }
     
     // MARK: - Helper Methods
+    
+    func setupViews() {
+        self.view.backgroundColor = .systemGray6
+        
+        self.navigationItem.title = "Entries"
+        self.navigationItem.rightBarButtonItem = addEntryButton
+    }
+    
+    func configureViews() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    // MARK: - Views
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    let addEntryButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.tintColor = brandColor
+        button.image = UIImage(systemName: "plus")
 
+        return button
+    }()
+
+}
+
+extension EntryListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
