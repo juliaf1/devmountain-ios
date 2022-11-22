135ee50087915f2f543afa2f1859fb8ad0ac0855
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-22 13:26:26 -0300

    chore(unit5-contacts): (create app with mvc folder structure)

---
+++ unit5/00-ios-skills-contacts/Contacts/Contacts/Models/HelperPlaceholder.swift
@@ -0,0 +1,8 @@
+//
+//  HelperPlaceholder.swift
+//  Contacts
+//
+//  Created by Julia Frederico on 22/11/22.
+//
+
+import Foundation
---
+++ unit5/00-ios-skills-contacts/Contacts/Contacts/Models/ModelPlaceholder.swift
@@ -0,0 +1,8 @@
+//
+//  ModelPlaceholder.swift
+//  Contacts
+//
+//  Created by Julia Frederico on 22/11/22.
+//
+
+import Foundation
