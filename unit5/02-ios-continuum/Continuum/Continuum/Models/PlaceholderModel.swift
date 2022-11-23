de1e49bb52753e14830bf9b8fc58390fef7e8dc8
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-23 18:23:31 -0300

    feat(unit5-continuum): (add post class model)

--- unit5/02-ios-continuum/Continuum/Continuum/Models/PlaceholderModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PlaceholderModel.swift
-//  Continuum
-//
-//  Created by Julia Frederico on 22/11/22.
-//
-
-import Foundation
---
+++ unit5/02-ios-continuum/Continuum/Continuum/Models/Post.swift
@@ -0,0 +1,40 @@
+//
+//  Post.swift
+//  Continuum
+//
+//  Created by Julia Frederico on 23/11/22.
+//
+
+import UIKit
+
+class Post {
+
+    // MARK: - Stored Properties
+
+    let timestamp: Date
+    let caption: String
+    var comments: [Comment]
+    
+    var photoData: Data?
+
+    // MARK: - Computed Properties
+
+    var photo: UIImage? {
+        get {
+            guard let photoData = photoData else {
+                return nil
+            }
+            return UIImage(data: photoData)
+        } set {
+            self.photoData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    init(photo: UIImage, caption: String, comments: [Comment], timestamp: Date = Date()) {
+        self.caption = caption
+        self.comments = comments
+        self.timestamp = timestamp
+        self.photo = photo
+    }
+    
+}
