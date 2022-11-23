22d7cd113bebdb2595db05fbc7c3aa41b118e53e
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-23 12:03:16 -0300

    feat(unit5-contact): (implement fetch contacts function)

--- unit5/00-ios-skills-contacts/Contacts/Contacts/Controllers/ModelControllers/ContactController.swift
+++ unit5/00-ios-skills-contacts/Contacts/Contacts/Controllers/ModelControllers/ContactController.swift
@@ -25,7 +25,24 @@ class ContactController {
     // MARK: - Helpers Methods
     
     func fetchContacts(completion: @escaping (Error?) -> Void) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: ContactKeys.recordType, predicate: predicate)
         
+        privateDB.fetch(withQuery: query) { result in
+            switch result {
+            case .success(let successResult):
+                successResult.matchResults.forEach { matchTuple in
+                    if case .success(let record) = matchTuple.1 {
+                        guard let contact = Contact(ckRecord: record) else { return }
+                        self.contacts.append(contact)
+                    }
+                }
+
+                return completion(nil)
+            case .failure(let error):
+                return completion(error)
+            }
+        }
     }
     
     func createContact(name: String, phone: String?, email: String?, photo: UIImage?, completion: @escaping (Error?) -> Void) {
