532256a3b3587b3e227b6451b8cc094305382246
Julia Frederico <magalhaesjulia@id.uff.br>
2022-11-29 20:29:18 -0300

    feat(unit5-continuum): (adds ck record convenience initializers to comment model)

--- unit5/02-ios-continuum/Continuum/Continuum/Models/Comment.swift
+++ unit5/02-ios-continuum/Continuum/Continuum/Models/Comment.swift
@@ -6,6 +6,16 @@
 //
 
 import Foundation
+import CloudKit
+
+struct CommentKeys {
+    
+    static let recordType = "Comment"
+    static let recordID = "recordID"
+    fileprivate static let text = "text"
+    fileprivate static let timestamp = "timestamp"
+    
+}
 
 class Comment {
     
@@ -13,12 +23,35 @@ class Comment {
     
     let text: String
     let timestamp: Date
+    let recordID: CKRecord.ID
     
     // MARK: - Initializer
     
-    init(text: String, timestamp: Date = Date()) {
+    init(text: String, timestamp: Date = Date(), recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.text = text
         self.timestamp = timestamp
+        self.recordID = recordID
+    }
+    
+    convenience init?(ckRecord: CKRecord) {
+        guard let text = ckRecord[CommentKeys.text] as? String,
+              let timestamp = ckRecord[CommentKeys.timestamp] as? Date,
+              let recordID = ckRecord[CommentKeys.recordID] as? CKRecord.ID else { return nil }
+        
+        self.init(text: text, timestamp: timestamp, recordID: recordID)
+    }
+    
+}
+
+extension CKRecord {
+    
+    convenience init(comment: Comment) {
+        self.init(recordType: CommentKeys.recordType)
+        
+        self.setValuesForKeys([
+            CommentKeys.text: comment.text,
+            CommentKeys.timestamp: comment.timestamp,
+        ])
     }
     
 }
